c184acb4df5f63af0f4855fe61c93c22fb39c18e
Karen <karen.czock@bsbkoeln.de>
2023-04-13 21:23:52 +0200

    update: course info

--- src/components/sessions/Sessions1.vue
+++ src/components/sessions/Sessions1.vue
@@ -6,26 +6,24 @@
     <h3 class="text-sm filter-outline-xs mt-10 mb-3">1. Introducing Ourselves</h3>
     <p>
     What are your coding skills? Have you ever build or designed a webpage?<br>
-    What is your favourite or your least favourite website?
+    What is (one of) your most used website(s)?<br>
     What do you wish from this course?
     </p>
 
     <!-- 10:00–10:30 -->
     <h3 class="text-sm filter-outline-xs mt-10 mb-3">2. Introducing the Course</h3>
 
-    <p>This course aims at providing basic understanding of web development. For this purpose, on the technical side, we will start with a very basic introduction to HTML and CSS. We will not have a fully extensive course on everything there is to know. Instead, we want learn together how to extend our knowledge from there and share useful resources.</p>
-    <p>Content-wise, we will investigate how the current web and its conventions are shaped by template culture. We will approximate this topic in small code experiments, investigating reoccuring website elements. In the end, these small experiments will compile into a collective, alternative website template. Thereby, we want to attain an understanding for contemporary discussion revolving around the web, for different tools and for the context in which websites are published.</p>
-    <p>Learning from each other and sharing knowledge is an important part of the course. Every code snippet and observeration is therefore considered open-source and free for everyone else to use. We also try to make everything happen within the predefined course times – no homeworks.</p>
+    <p>This course aims at providing an introduction to web development. For this purpose, on the technical side, we will start with a very basic introduction to HTML and CSS. We will not learn best practices or cover both subjects extensively. Instead, we want lose fears and share useful resources to extend our knowledge from there.</p>
+    <p>Content-wise, we will investigate how the current web and its conventions are shaped by template culture and ever-evolving tools to create websites without writing a line of code. Discussing contemporary point of views and trying out different tools we want to develop a feeling for the context into which a website is published.  Small code experiments examining the history, functionality and visuality of common website elements will in the end compile into a collective, dysfunctional website template.</p>
+    <p>Learning from each other and sharing knowledge is an important part of the course. Every code snippet and observeration is considered open-source and free for everyone to use. We also try to make everything happen within the predefined course times – no homeworks.</p>
     
     <!-- 10:30–11:00 -->
     <h3 class="text-sm filter-outline-xs mt-10 mb-3">3. Code Editor Setup </h3>
     <ol class="list-decimal px-10 py-3 border border-secondary rounded-md mx-3">
         <li>Start <a target="_blank" href="https://code.visualstudio.com/">VS Code</a></li>
         <li>Install <a target="_blank" href="https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer">VS Code Live Server</a></li>
-        <li>Download the <a target="_blank" download href="https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer">HTML boilderplate setup</a></li>
-        <li>Duplicate the folder and give it a new name</li>
-        <li>Open the folder in VS code</li>
-        <li>Start a live server</li>
+        <li>Download the <a target="_blank" download href="https://github.com/czkaa/HTML-boilerplate">HTML boilderplate setup</a></li>
+        <li>Open VS Code</li>
     </ol>
 
     <!-- 11:00–12:00, 12:00–13:00 -->
@@ -39,8 +37,8 @@
     
     <div class="border border-secondary p-3 my-3 mx-6 rounded-md">
         <ol class="list-decimal px-6">
-            <li>Duplicate the boilerplate and give it a new name</li>
-            <li>Find 5 different tags <a href="https://htmlreference.io/" target="_blank">here</a> and paste them in your own HTML document</li>
+            <li>Create an index.html file</li>
+            <li>Find 5 different tags (which you have not yet used)<a href="https://htmlreference.io/" target="_blank">here</a> and paste them in your own HTML document</li>
             <li>Add some example content</li>
             <li>Check how their default style looks in different browsers</li>
             <li>Explain them to your neighbour</li>
@@ -50,8 +48,9 @@
     <div class="border border-secondary p-3 my-3 mx-6 rounded-md">
         <ol class="list-decimal px-6">
             <li>Duplicate the boilerplate and give it a new name</li>
-            <li>Chose a song, a short text or a poem which you like</li>
-            <li>Find the text snippet and paste it into the index file</li>
+            <li>Start a live server</li>
+            <li>Choose a song, a short text or a poem which you like</li>
+            <li>Find the text snippet and paste it into the index.html file</li>
             <li>Illustrate the text using only HTML elements. For example, use <a target="_blank" href="https://htmlreference.io/element/table/">tables</a>, <a target="_blank" href="https://htmlreference.io/element/h1/">headlines</a>, <a target="_blank" href="https://htmlreference.io/element/input/">input fields</a>, <a target="_blank" href="https://htmlreference.io/element/li/">lists</a>, <a target="_blank" href="https://htmlreference.io/element/summary/">summarys</a>.</li>
             <li>Open your website in different browsers and check if it looks different</li>
             <li>Present your website to the group</li>
@@ -76,12 +75,12 @@
 
     <div class="border border-secondary p-3 my-3 mx-6 rounded-md">
         <ol class="list-decimal px-6">
-            <li>Go to your favorite website</li>
+            <li>Go to your most/often used favorite website</li>
             <li>Open the inspector</li>
             <li>Change the look of as many elements as possible by changing the <a target="_blank" href="https://cssreference.io/property/background-color/">background color</a>, the <a target="_blank" href="https://cssreference.io/property/border/">border</a> and the <a target="_blank" href="https://cssreference.io/property/border-radius/">border radius</a>.
                 <i>Hint: Don't reload the browser or your changes will get lost.</i></li>
             <li>Advanced: Add <a target="_blank" href="https://cssreference.io/property/box-shadow/">box shadows</a>. You can generate cool box shadows with <a target="_blank" href="https://html-css-js.com/css/generator/box-shadow/">this generator</a>.</li>
-            <li>Document the results with screenshots</li>
+            <li>Document the results with screenshots when it feels like you are done</li>
             <li>Add the screenshots to our <a href="https://www.are.na/karen-czock/dt-output">are.na output channel</a></li>
         </ol>
     </div>
@@ -91,7 +90,8 @@
             <li>Look around</li>
             <li>Pick a view, an object or an arrangement of objects which you find interesting</li>
             <li>Duplicate the boilerplate and give it a new name</li>
-            <li>Try to recreate it using only HTML and CSS. Check out different ways to position your elements <a target="_blanl" href="https://www.are.na/block/21328405">here</a></li>
+            <li>Start a live server</li>
+            <li>Try to recreate it using only HTML and CSS. Check out different ways to position your elements <a target="_blanl" href="https://learnlayout.com/5">here</a></li>
             <li>Advanced: Make your website interactive using <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/:hover?retiredLocale=de">hover</a></li>
             <li>Advanced: Make your website responsive using <a target="_blank" href="https://www.w3schools.com/css/css_rwd_mediaqueries.asp">media queries</a></li>
             <li>Present your website to the group</li>
