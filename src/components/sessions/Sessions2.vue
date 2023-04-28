813bf8b998e6f005e720b3cbd8695206afc6ef14
Karen <karen.czock@bsbkoeln.de>
2023-04-28 09:54:49 +0200

    update

--- src/components/sessions/Sessions2.vue
+++ src/components/sessions/Sessions2.vue
@@ -8,48 +8,54 @@
     </p>
 
 
-    <h3 class="text-md filter-outline-xs mt-16 mb-3">1. Collective Reading </h3>
+    <h3 class="text-md filter-outline-xs mt-16 mb-3">2. Collective Reading </h3>
       <ol class="list-decimal px-10 py-3 border border-secondary rounded-md mx-3">
-         <li>Read the Text <a target="_blank" href="">Why Do All Websites Look the Same?</a> by Boris Müller</li>
+         <li>Read the Text <a target="_blank" href="https://docs.google.com/document/d/1hvWlene4XhtVg_gPiAGYT5ZNYhlvzhBY4S284hFDdZI/edit#heading=h.wilshhi16ig">Why Do All Websites Look the Same?</a> by Boris Müller</li>
          <li>Leave comments where you have a question, a link, an observation or anything you wish to share</li>
       </ol>
 
-    <h3 class="text-md filter-outline-xs mt-16 mb-3">2. Hands-on Templating </h3>
+    <!-- <h3 class="text-md filter-outline-xs mt-16 mb-3">2. Hands-on Templating </h3>
     <ol class="list-decimal px-10 py-3 border border-secondary rounded-md mx-3">
+        Having discussed what the web looks like, we want to investigate the software behind so many websites and  
         <li>Find your name on this <a target="_blank" download href="">list</a>. Open the link and log in with the provided password.</li>
         <li>Explore the interface and the option set of the website builder. Take screenshots of reoccuring elements and anything you find remarkable and add them to our <a href="https://www.are.na/karen-czock/dt-output">are.na output channel</a>. You can also write down observations.</li>
-        <li>Pick sentences and comments from the <a target="_blank" href="">text we have read in the morning</a>.</li>
+        <li>Pick sentences and comments from the <a target="_blank" href="https://docs.google.com/document/d/1hvWlene4XhtVg_gPiAGYT5ZNYhlvzhBY4S284hFDdZI/edit#heading=h.wilshhi16ig">text we have read in the morning</a>.</li>
         <li>Place the text on the blank website using the template elements. How messy can it get? How can you misuse the provided blocks? <i>Hint: Dont spend too much time trying to understand the interface – this is supposed to be a quick experiment!</i></li>
         <li>Add screenshots to our <a href="https://www.are.na/karen-czock/dt-output">are.na output channel</a></li>
-    </ol>
+    </ol> -->
 
-    <h3 class="text-md filter-outline-xs mt-16 mb-3">3. Codepen Setup </h3>
+    <h3 class="text-md filter-outline-xs mt-16 mb-3">3. A Short Introduction to Are.na</h3>
+
+    <h3 class="text-md filter-outline-xs mt-16 mb-3">4. JSFiddle Setup </h3>
     <ol class="list-decimal px-10 py-3 border border-secondary rounded-md mx-3">
-        <li>Go to <a target="_blank" href="https://codepen.io/">Codepen and create an account</a></li>
-        <li>Create a new pen</li>
-        <li>Type some HTML and CSS and see what happens</li>
+        <li>Go to <a target="_blank" href="https://jsfiddle.net/">JS Fiddle</a></li>
+        <li>Type some HTML and CSS. Save and see what happens </li>
+        <li>Click on <i>Collaborate</i> and invite the person next to you to code</li>
+        <li>Code a little message and save the fiddle</li>
     </ol>
 
-
-
-    <h3 class="text-md filter-outline-xs mt-16 mb-3">4. A Short Introduction to CSS II </h3>
+    <h3 class="text-md filter-outline-xs mt-16 mb-3">5. A Short Introduction to CSS II </h3>
 
     <ol class="mt-3 text-xs">
         <h4 class="text-sm filter-outline-xs">Tutorials</h4>
         <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/CSS_basics" target="_blank">CSS Basics</a></li>
-        <li><a href="https://learnlayout.com/" target="_blank">CSS Layout</a></li>
         <li><a href="https://cssreference.io/" target="_blank">A List of CSS Attributes</a></li>
+        <li><a href="https://learnlayout.com/" target="_blank">CSS Layout</a></li>
+        <li><a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank">A Complete Guide to Flexbox</a></li>
+        <li><a href="https://css-tricks.com/snippets/css/complete-guide-grid/" target="_blank">A Complete Guide to Grid</a></li>
+
     </ol>
 
     <div class="border border-secondary p-3 my-3 mx-6 rounded-md">
         <ol class="list-decimal px-6">
+            In this exercise, we want to apply and deepen what we have learned so far about HTML and CSS. 
             <li>Check out the items arranged at the center of the room</li>
-            <li>Check out the Codepen Starterkits <a target="_blanl" href="https://www.are.na/karen-czock/dt-starterkits">Codepen starterkits. They contain different starting points to position elments on a webpage as well as some sample CSS.</a></li>
-            <li>Pick one of the starterkits and fork it or open an empty, new pen</li>
-            <li>Try to recreate the still life using only HTML and CSS. Check out different ways to position your elements <a target="_blanl" href="https://learnlayout.com/5">here</a></li>
+            <li>Check out the <a target="_blank" href="https://www.are.na/karen-czock/dt-starterkits">JSFiddle Starterkits.</a> They contain different starting points to position elments on a webpage as well as some sample CSS.</li>
+            <li>Pick one of the starterkits and fork it or open an empty fiddle</li>
+            <li>Try to recreate the still life using only HTML and CSS.</li>
             <li>Advanced: Make your sketch interactive using <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/:hover?retiredLocale=de">hover</a></li>
             <li>Advanced: Make your sketch responsive using <a target="_blank" href="https://www.w3schools.com/css/css_rwd_mediaqueries.asp">media queries</a></li>
-            <li>Add screenshots to our <a href="https://www.are.na/karen-czock/dt-output">are.na output channel</a></li>
+            <li>Add the link to your fiddle to the <a href="https://www.are.na/karen-czock/dt-output">are.na output channel</a></li>
         </ol>   
     </div>
 
@@ -60,9 +66,6 @@
         <li><a href="https://www.eggalone.com/" target="_blank">https://www.eggalone.com/</a></li>
         <li><a href="https://root.schloss-post.com/" target="_blank">https://root.schloss-post.com/</a></li>
     </ol>
-
-    <h3 class="text-md filter-outline-xs mt-16 mb-3">5. A Short Introduction to Are.na</h3>
-
   
     <h3 class="text-md filter-outline-xs mt-16 mb-3">6. Further Reading</h3>
 
