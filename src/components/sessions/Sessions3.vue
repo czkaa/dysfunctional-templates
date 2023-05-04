9e22c93faa2f5819e09a918fc03815ae12169287
Karen <karen.czock@bsbkoeln.de>
2023-05-04 21:35:55 +0200

    upadete

--- src/components/sessions/Sessions3.vue
+++ src/components/sessions/Sessions3.vue
@@ -14,18 +14,18 @@
          <li>Leave comments where you have a question, a link, an observation or anything you wish to share</li>
       </ol>
 
-    <h3 class="text-md filter-outline-xs mt-16 mb-3">3. Experimenting</h3>
+    <h3 class="text-md filter-outline-xs mt-16 mb-3">3. Talking about Tools</h3>
 
     <div class="border border-secondary p-3 my-3 mx-6 rounded-md">
         <ol class="list-decimal px-6">
          <h4 class="text-sm filter-outline-xs">Three-Words Websites</h4>
-           Talking about tools, we want to try out what seems to be the opposite of hand-made: Using an AI to generate a website. 
+            How far can we get without using our hands?
             <li>Write down three meaningful words – adjectives or nouns – and hand them to your left-hand neighbour</li>
             <li>Go to <a href="https://chat.openai.com/auth/login">Chat GPT</a> and start a new chat. If you haven't used it yet and don't want to create an account you can use a<a href="https://tempr.email/"> dumpster e-mail </a></li>
             <li>Try to make ChatGPT write you the code for a website that takes the three words as a theme, atmosphere or topic. <i>Hint: Usually it is best to start with a simple promt, e.g. "Can you provide the HTML and CSS Code for a Website that...".</i> </li>
             <li>Copy the HTML and CSS to <a target="_blank" href="https://jsfiddle.net/">JS Fiddle</a> and see how it looks.</li>
-            <li>Try to get ChatGTP to make the fiddle e.g. interactive, more colorful, responsive, add backgrounds, shadows etc. without writing a line of code yourself.</li>
-            <li>Add the link to your fiddle to the <a href="https://www.are.na/karen-czock/dt-output">are.na output channel when you feel like you are done</a></li>
+            <li>Try to get ChatGTP to make the HTML and CSS e.g. interactive, more colorful, responsive, add backgrounds, shadows etc. without writing a line of code yourself.</li>
+            <li>Add the link to your fiddle to the <a href="https://www.are.na/karen-czock/dt-output">are.na output channel</a> when you feel like you are done</li>
         </ol>   
     </div>
 
@@ -37,24 +37,28 @@
 
     <ol class="mt-3 text-xs">
         <h4 class="text-sm filter-outline-xs">Tutorials</h4>
-        <li><a href="https://learnlayout.com/" target="_blank">CSS Layout</a></li>
-        <li><a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank">A Complete Guide to Flexbox</a></li>
-        <li><a href="https://css-tricks.com/snippets/css/complete-guide-grid/" target="_blank">A Complete Guide to Grid</a></li>
-        <li><a href="https://www.w3schools.com/css/css_boxmodel.asp" target="_blank">Margin, Padding and Borders</a></li>
-        <li><a href="https://www.w3schools.com/cssref/css_units.php/" target="_blank">CSS Units</a></li>
+        <li><a href="" target="_blank">CSS Layout</a></li>
+        <li><a href="" target="_blank"></a></li>
+        <li><a href="" target="_blank"></a></li>
+        <li><a href="" target="_blank"></a></li>
+        <li><a href="" target="_blank"></a></li>
     </ol>
 
     <div class="border border-secondary p-3 my-3 mx-6 rounded-md">
         <ol class="list-decimal px-6">
-         <h4 class="text-sm filter-outline-xs">Structures</h4>
-            <li></li>
+         <h4 class="text-sm filter-outline-xs">Structures (<i><a target="_blank" href="https://handmade-web.net/projects/structure.html">from here</a></i>)</h4>
+            In this exercise we want to get a better understanding of how elements can be organized on a webpage.
+            <li>Start by thinking of a concrete metaphor/analogue to build your site around. Is the site a building, a map, a garden, a piece of clothing? Does it exist in real life or do you imagine it?</li>
+            <li>Make a wireframe – a small sketch of the structure of your website – with pen and paper. Is it a grid or is it organic? Try to keep it simple, abstract, and narrow it down to a few elements. </li>
+            <li>Start to build the strucutre in a <a target="_blank" href="https://jsfiddle.net/">JS Fiddle</a>. Is it scrollable? What happens, when I resize the window? </li>
+            <li>Style your webite. How can I add materiality within a browser?</li>
             <li>Add the link to your fiddle to the <a href="https://www.are.na/karen-czock/dt-output">are.na output channel</a></li>
         </ol>   
     </div>
 
     <ol class="mt-3 text-xs">
         <h4 class="text-sm filter-outline-xs">Collection</h4>
-        <li><a href="https://www.thisgrey.com/" target="_blank">??</a></li>
+        <li><a href="https://gardenblues.net/" target="_blank">Garden Blues</a></li>
     </ol>
   
     <h3 class="text-md filter-outline-xs mt-16 mb-3">6. Further Reading</h3>
