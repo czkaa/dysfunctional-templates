2f0990e9b1d7822c9cd2de2942432efca8455a5e
Karen <karen.czock@bsbkoeln.de>
2023-06-09 09:24:44 +0200

    update

--- src/components/sessions/Sessions6.vue
+++ src/components/sessions/Sessions6.vue
@@ -1,5 +1,24 @@
 <template>
    <section session-5 id="session-5" class="p-3 space-y-3 border-b border-secondary">
     <h2 class="text-lg text-center italic leading-none filter-outline">Collective Coding</h2>
+
+    <h3 class="text-md filter-outline-xs mt-16 mb-3">1. Catch-up </h3>
+    <p class="block">
+      Is there any thought, question or proposal that came up after the last session? How do we want to handle the Brückentag (May 19th)?
+    </p>
+
+
+    <h3 class="text-md filter-outline-xs mt-16 mb-3">2. Collective Reading </h3>
+    <p class="block">
+      (to be discussed)
+    </p>
+   <ol class="list-decimal px-10 py-3 border border-secondary rounded-md mx-3">
+      <li>Read the Text <a target="_blank" href="https://thecreativeindependent.com/essays/laurel-schwulst-my-website-is-a-shifting-house-next-to-a-river-of-knowledge-what-could-yours-be/">My website is a shifting house next to a river of knowledge. What could yours be?</a> by Laurel Schwulst.</li>
+      <li>Leave comments where you have a question, a link, an observation or anything you wish to share</li>
+   </ol>
+
+   <h3 class="text-md filter-outline-xs mt-16 mb-3">3. Collective Coding</h3>
+
+
    </section>
 </template>
